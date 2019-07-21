d33535eb881eb3cd9ae9d2dab1b9773b989877e7
zkl <zhangkaileina@163.com>
2019-07-21 15:41:26 +0800

    Update run_spiders.py

--- core/proxy_spider/run_spiders.py
+++ core/proxy_spider/run_spiders.py
@@ -1,3 +1,6 @@
+from settings import  PROXIES_SPIDERS
+import importlib
+
 """
 实现爬虫运行模块：
 目标：根据配置文件信息，加载爬虫，抓代理IP，进行校验，如果可用，写入数据库中
@@ -13,4 +16,34 @@
 """
 
 class RunSpiders(object):
-    
+
+    def get_spider_from_settings(self):
+         # 2.1根据配置文件信息，获取爬虫对象列表
+    #         遍历配置文件中的爬虫信息，获取每个爬虫全类名
+        for full_class_name in PROXIES_SPIDERS:
+            # 'core.proxy_spider.proxy_spiders.Ip66Spider',
+    #         获取模块名和类名
+            module_name,class_name = full_class_name.rsplit('.',maxsplit=1)
+    #         根据模块名和类名导入类
+            module = importlib.import_module(module_name)
+    #         根据类名，从模块中，获取类
+            cls = getattr(module,class_name)
+    #         创建爬虫对象
+            spider = cls()
+            yield  spider
+
+
+    # 2.1根据配置文件信息，获取爬虫对象列表
+    def run(self):
+        spiders = self.get_spider_from_settings()
+        # 2.2遍历爬虫对象列表，获取爬虫对象，遍历爬虫对象的get_proxies方法，获取代理IP
+        for spider in spiders:
+#             遍历爬虫对象的get_proxies方法，获取代理IP
+            for proxy in spider.get_proxies():
+                print(proxy)
+
+     # 2.3检测代理Ip（代理Ip检测模块）
+                
+if __name__=='__main__':
+    rs = RunSpiders()
+    rs.run()
